15876baa9cb7f6b553bfb7717c03cdb8f8b43f5e
Moises Cruz <serviciopatrimonial@gmail.com>
2025-01-28 08:20:05 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+
+export default function App() {
+  return (
+    <div className=''>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </div>
+  )
+}
